d7286183ccf6026a8dfaad7f2dceebb2e09735c9
Riya Jain <jriya.shi@gmail.com>
2023-05-09 17:34:17 +0530

    KUCH NAHI HO SKTA

--- app/auth/QueryWrapper.tsx
+++ app/auth/QueryWrapper.tsx
@@ -1,19 +1,20 @@
-'use client'
-import {QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactNode } from "react"
-import {Toaster} from "react-hot-toast"
+"use client"
 
-interface Props {
-    children? : ReactNode
-}
+import React, { ReactNode } from "react"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { Toaster } from "react-hot-toast"
 
 const queryClient = new QueryClient()
 
-const QueryWrapper = ({children} : Props) => (
-    <QueryClientProvider client={queryClient}>
-        <Toaster />
-        {children}
-    </QueryClientProvider>
+interface Props {
+  children?: ReactNode
+}
+
+const QueryWrapper = ({ children }: Props) => (
+  <QueryClientProvider client={queryClient}>
+    <Toaster />
+    {children}
+  </QueryClientProvider>
 )
 
 export default QueryWrapper
