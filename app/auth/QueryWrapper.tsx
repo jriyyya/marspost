057cdf3a626d3ffde5545a3b25f442e7c001d082
Riya Jain <jriya.shi@gmail.com>
2023-05-09 15:29:51 +0530

    Add post frontend completed

--- app/auth/QueryWrapper.tsx
+++ app/auth/QueryWrapper.tsx
@@ -1,6 +1,7 @@
 'use client'
 import {QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactNode } from "react"
+import {Toaster} from "react-hot-toast"
 
 interface Props {
     children? : ReactNode
@@ -10,6 +11,7 @@ const queryClient = new QueryClient()
 
 const QueryWrapper = ({children} : Props) => (
     <QueryClientProvider client={queryClient}>
+        <Toaster />
         {children}
     </QueryClientProvider>
 )
